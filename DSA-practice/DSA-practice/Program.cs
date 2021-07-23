4a551814862b4bfdc10f372b2754f0581835740b
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2021-07-23 20:24:06 +0530

    first commit

---
+++ DSA-practice/DSA-practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSA_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
